d118830aa638d94385da6ab79683f993cf9d0159
drinko <tolstykh.mi@gmail.com>
2020-10-29 13:28:39 +0300

    Start app

---
+++ src/main/java/com/guide/currencyconverter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.guide.currencyconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
