81572b3ba94670419493404f3ad96bbf55bb6799
drinko <tolstykh.mi@gmail.com>
2020-10-31 17:19:12 +0300

    creat layout

--- app/src/main/java/com/guide/currencyconverter/views/MainActivity.kt
+++ app/src/main/java/com/guide/currencyconverter/views/MainActivity.kt
@@ -1,51 +1,115 @@
 package com.guide.currencyconverter.views
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.guide.currencyconverter.R
 import com.guide.currencyconverter.contract.ContractPresenter
 import com.guide.currencyconverter.contract.ContractView
 import com.guide.currencyconverter.presenters.MainPresenter
+import java.lang.reflect.Array
 
 
 class MainActivity : AppCompatActivity(), ContractView {
 
-    private val TAG = "MainActivity"
-
     private var mPresenter: ContractPresenter? = null
-    private var mButton: Button? = null
-    private var mTextView: TextView? = null
-
-
+    private var mSwapBtn: ImageButton? = null
+    private var mRefreshBtn: Button? = null
+    private var mLeftTextView: EditText? = null
+    private var mRightTextView: EditText? = null
+    private var leftSpinner: Spinner? = null
+    private var rightSpinner: Spinner? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mLeftTextView = findViewById(R.id.left_textbox_view)
+        mRightTextView = findViewById(R.id.right_textbox_view)
+        mSwapBtn = findViewById(R.id.swap_btn_view)
+//        mRefreshBtn = findViewById(R.id.button_view)
+
+        leftSpinner = findViewById(R.id.left_spinner_view)
+        rightSpinner= findViewById(R.id.right_spinner_view)
+
         mPresenter = MainPresenter(this)
 
-        mTextView = findViewById(R.id.text_view)
-        mButton = findViewById(R.id.button_view)
+        leftSpinner?.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
 
-        mButton?.setOnClickListener(object : View.OnClickListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                mPresenter!!.onChangeCurrencyFrom()
+            }
+
+        }
+        rightSpinner?.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                mPresenter!!.onChangeCurrencyTo()
+            }
+
+        }
+
+
+        mLeftTextView?.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                mPresenter!!.getCurrencyResult()
+            }
+
+        })
+
+        mSwapBtn?.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                (mPresenter as MainPresenter).onButtonWasClicked()
+                mPresenter!!.swapCurrency()
             }
 
         } )
+
+    }
+
+    override fun swapCurrency() {
+        var temp = leftSpinner!!.selectedItemPosition
+        leftSpinner?.setSelection(rightSpinner!!.selectedItemPosition)
+        rightSpinner?.setSelection(temp)
+
     }
 
     override fun showText(message: String?) {
-        mTextView?.setText(message)
-        Log.d(TAG, "showMessage()")
+        mRightTextView?.setText(message)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         mPresenter?.onDestroy()
-        Log.d(TAG, "onDestroy()")
+    }
+
+    override fun default(){
+        var currencyList = resources.getStringArray(R.array.Currency)
+        if( leftSpinner != null && rightSpinner != null ){
+            val adapter = ArrayAdapter(this, R.layout.spinner_row, currencyList)
+            leftSpinner!!.adapter = adapter
+            leftSpinner!!.setSelection(1)
+            rightSpinner!!.adapter = adapter
+        }
+    }
+
+    override fun getLeftCurrency(): String {
+        return leftSpinner?.getSelectedItem().toString()
+    }
+
+    override fun getRightCurrency(): String{
+        return rightSpinner?.getSelectedItem().toString()
+    }
+    override fun getCountCurrency(): String{
+        return mLeftTextView?.text.toString()
     }
 }
